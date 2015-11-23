ca257a049f6c1ca8e889fa69bad31536f85e6651
CristhianRuiz593 <cristhian.ruiz.gamboa@hotmail.com>
2015-11-23 18:29:49 -0500

    programacion_deberes

---
+++ Cristhian_Ruiz/src/Programacion_Clase_2/Circulo.java
@@ -0,0 +1,25 @@
+package Programacion_Clase_2;
+
+public class Circulo {
+	/*crear una clase circulo con el atributo radio, y el metodo getArea(). crear dos
+	 * constructores, el primero sin argumentos, el segundo con el parametro de entrada*/
+	 
+	private double radio;
+	
+	public Circulo(){	
+	}
+	public Circulo(double radioParam){
+	this.radio=radioParam;	
+	}
+	public void setRadio(double radioParam){
+	this.radio=radioParam;
+	}
+	public double getRadio(){
+	return this.radio;
+	}
+	public double getArea(){
+	return Math.PI*radio*radio;
+}
+
+	}
+
---
+++ Cristhian_Ruiz/src/Programacion_Clase_2/Ejercicio_2.java
@@ -0,0 +1,14 @@
+package Programacion_Clase_2;
+
+public class Ejercicio_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Descuento des1=new Descuento(0.1,100);
+		des1.setTotalcompra(150);
+		
+		double total=des1.getTotal();
+		System.out.println("totalcompra: "+total);
+		System.out.println("totalcompra: "+des1.getTotal());
+}
+}
